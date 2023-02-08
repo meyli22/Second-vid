3db2466cfb46cd1c93a4f9c52a78c861e71ee0c7
meyli22 <122221622+meyli22@users.noreply.github.com>
2023-02-08 08:36:38 +0700

    Update routes.py

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -11,12 +11,17 @@ import os
 
 
 @app.route('/admin')
+#creates a route for the Flask web application. When a user visits the '/admin' URL, the admin() function is executed
 def admin():
     if 'email' not in session:
+#checks if the 'email' key exists in the current session. If not, a message with a danger level is flashed 
         flash(f'Please to continue login first', 'danger')
         return redirect(url_for('login'))
+#the user is redirected to the 'login' URL
     products = Addproduct.query.all()
+#if the user is logged in, the function fetches all products from the database and passes the data to the render method 
     return render_template('admin/index.html', title='Admin Page', products=products)
+#the template at 'admin/index.html' is rendered and returned to the user's browser.
 
 # @app.route('/admin')
 # def admin():
@@ -25,32 +30,45 @@ def admin():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
+    if form.validate_on_submit():
+#validates the data submitted in the form 
         hash_password = bcrypt.generate_password_hash(form.password.data)
+#if the form data is valid, a password hash is generated and a new user is created
 
         user = User(name=form.name.data, username=form.username.data, email=form.email.data, password=hash_password)
         db.create_all()
         db.session.add(user)
         db.session.commit()
+#new user is added to the database session and committed
         session['email'] = form.email.data
+#user's email is stored in a session variable
         flash(f' Welcome {form.name.data} !Thank you for registering', 'success')
         return redirect(url_for('login'))
+#success message is displayed after the user has registered, and they are redirected to the login page.
+
     return render_template('admin/register.html', form=form, title="Registeration page")
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    form = LoginForm(request.form)
-    if request.method == "POST" and form.validate():
-        user = User.query.filter_by(email = form.email.data).first()
+    form = LoginForm()
+    if form.validate_on_submit():
+#form is validated when the user submits it
+        user = User.query.filter_by(email=form.email.data).first()
+#the entered email is used to search for a user in the database.
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            paas = bcrypt.check_password_hash(user.password, form.password.data)
-            print(paas)
+#if a matching user is found, the entered password is checked against the hashed 
+#password stored in the database 
             session['email'] = form.email.data
-            flash(f'Welcome {form.email.data} You are logged in', 'success')
-            return redirect(request.args.get('next') or url_for('admin'))
+            flash(f'welcome {form.email.data} you are logedin now','success')
+            return redirect(url_for('admin'))
+#if the password matches, the user is logged in and redirected to the "admin" page
         else:
-            flash('wrong password. please try again', 'danger')
-    return render_template('admin/login.html', form=form,title='Login Page')
+#if the password does not match or the user is not found, a message is displayed 
+#indicating that the login attempt was unsuccessful
+            flash(f'Wrong email and password', 'success')
+            return redirect(url_for('login'))
+    return render_template('admin/login.html',title='Login page',form=form)
+#returns a template for the login page with the form
     
     # if request.method=="POST" and form.validate():
     #     email = request.form.get('email')
